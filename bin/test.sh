0821c6a1486f18818b4523e24efbae9f684e966d
Victor Wibisono <vwibisono@dius.com.au>
2017-02-10 09:55:10 +0800

    Put test code into shell script.

--- bin/test.sh
+++ bin/test.sh
@@ -5,3 +5,7 @@
 
 # JSHint
 ./node_modules/jshint/bin/jshint 1-text2obj/index.js
+
+# Unit test
+echo 'Running unit tests'
+./node_modules/mocha/bin/mocha 3-mocha/currying.js --compilers js:babel-register
